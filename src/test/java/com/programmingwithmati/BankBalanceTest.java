6f5705145d5e0f284afb5d11c117d7630a82faf7
parikshit.deshpande <parikshit.deshpande@globant.com>
2024-05-27 13:34:16 +0530

    fix: refactoring code replace if with switch case

--- src/test/java/com/programmingwithmati/BankBalanceTest.java
+++ src/test/java/com/programmingwithmati/BankBalanceTest.java
@@ -7,43 +7,41 @@ import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class BankBalanceTest {
 
+    @Test
+    void testDeposit() {
+        var bankBalance = new BankBalance("acc-1", BigDecimal.TEN);
+        var transaction = new Transaction("t-1", "acc-1", BigDecimal.TEN, TransactionType.DEPOSIT);
+        var newBalance = bankBalance.processTransaction(transaction);
 
-  @Test
-  void testDeposit() {
-    var bankBalance = new BankBalance("acc-1", BigDecimal.TEN);
-    var transaction = new Transaction("t-1", "acc-1", BigDecimal.TEN, TransactionType.DEPOSIT);
-    var newBalance = bankBalance.processTransaction(transaction);
+        assertEquals(BigDecimal.valueOf(20), newBalance.amount());
+    }
 
-    assertEquals(BigDecimal.valueOf(20), newBalance.amount());
-  }
+    @Test
+    void testTransactionIsForDifferentAccount() {
+        var bankBalance = new BankBalance("acc-1", BigDecimal.TEN);
+        var transaction = new Transaction("t-1", "acc-2", BigDecimal.TEN, TransactionType.DEPOSIT);
+        assertThrows(InvalidAccountException.class, () -> bankBalance.processTransaction(transaction));
+    }
 
-  @Test
-  void testTransactionIsForDifferentAccount() {
-    var bankBalance = new BankBalance("acc-1", BigDecimal.TEN);
-    var transaction = new Transaction("t-1", "acc-2", BigDecimal.TEN, TransactionType.DEPOSIT);
-    assertThrows(InvalidAccountException.class, () -> bankBalance.processTransaction(transaction));
+    @Test
+    void givenValidAmount_whenWithdraw_thenWithdrawTheAmountFromBalance() {
+        var bankBalance = new BankBalance("acc-1", BigDecimal.valueOf(100));
+        var transaction = new Transaction("t-1", "acc-1", BigDecimal.TEN, TransactionType.WITHDRAW);
+        var newBalance = bankBalance.processTransaction(transaction);
 
-  }
+        assertEquals(BigDecimal.valueOf(90), newBalance.amount());
+    }
 
+    @Test
+    void givenInValidAmount_whenWithdraw_thenExpectException() {
+        var bankBalance = new BankBalance("acc-1", BigDecimal.valueOf(100));
+        var transaction = new Transaction("t-1", "acc-1", BigDecimal.valueOf(200.00), TransactionType.WITHDRAW);
 
-  @Test
-  void givenValidAmount_whenWithdraw_thenWithdrawTheAmountFromBalance(){
-    var bankBalance = new BankBalance("acc-1", BigDecimal.valueOf(100));
-    var transaction = new Transaction("t-1", "acc-1", BigDecimal.TEN, TransactionType.WITHDRAW);
-    var newBalance = bankBalance.processTransaction(transaction);
-
-    assertEquals(BigDecimal.valueOf(90), newBalance.amount());
-  }
-
-  @Test
-  void givenInValidAmount_whenWithdraw_thenExpectException(){
-    var bankBalance = new BankBalance("acc-1", BigDecimal.valueOf(100));
-    var transaction = new Transaction("t-1", "acc-1", BigDecimal.valueOf(200.00), TransactionType.WITHDRAW);
-
-    assertThrows(NotEnoughFundsException.class, () -> bankBalance.processTransaction(transaction));
-  }
+        assertThrows(NotEnoughFundsException.class, () -> bankBalance.processTransaction(transaction));
+    }
 }
